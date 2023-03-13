30f941891fa27ef9dcc193c5c8c80ddea5279acc
Edgar Avila <edgaravila01@yahoo.com>
2023-03-12 19:54:03 -0700

    phase 0 intro to js 2 looping

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Olie", "Aki"];
+const messages = [];
+
+function writeCards(name, eventNames) {
+    for (let i = 0; i < name.length; i++) {
+        let message = `Thank you, ${name[i]}, for the wonderful ${eventNames} gift!`;
+        messages.push(message);
+    }
+    return messages;
+}
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number --;
+    }
+}
